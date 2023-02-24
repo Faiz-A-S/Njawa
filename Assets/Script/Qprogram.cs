efd30f4724f8f3c83ca63efdb3151fabf4edfda3
Faiz-A-S <faizasucipto@gmail.com>
2023-02-24 20:22:35 +0700

    Atleast all good

--- Assets/Script/Qprogram.cs
+++ Assets/Script/Qprogram.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using TMPro;
 
 using PDollarGestureRecognizer;
 using QDollarGestureRecognizer;
@@ -14,6 +15,7 @@ public class Qprogram : MonoBehaviour
     private int strokeId = -1;
 
     private Vector3 virtualKeyPosition = Vector2.zero;
+    [SerializeField] private RectTransform drawRectUI;
     private Rect drawArea;
 
     private List<LineRenderer> gestureLinesRenderer = new List<LineRenderer>();
@@ -22,19 +24,17 @@ public class Qprogram : MonoBehaviour
     private int vertexCount = 0;
     private RuntimePlatform platform;
 
-    //GUI
-    private string message;
-    private bool recognized;
-    private string newGestureName = "";
-
     public string gestureResult = "";
+
+    [SerializeField] private TextMeshProUGUI gestureName;
+    [SerializeField] private string newGestureName;
 
 
     // Start is called before the first frame update
     void Start()
     {
         platform = Application.platform;
-        drawArea = new Rect(0, 0, Screen.width - Screen.width / 3, Screen.height);
+
         TextAsset[] gestureFolders = Resources.LoadAll<TextAsset>("GestureSet/10-stylus-MEDIUM/");
         foreach (TextAsset gestureXml in gestureFolders)
         {
@@ -43,9 +43,18 @@ public class Qprogram : MonoBehaviour
     }
 
     // Update is called once per frame
-    [Obsolete]
     void Update()
     {
+        // Get the panel's position and size
+        Vector2 panelPosition = drawRectUI.position;
+        Vector2 panelSize = drawRectUI.sizeDelta;
+
+        // Create a rectangle using the panel's position and size
+        drawArea = new Rect(panelPosition.x - panelSize.x / 2f,
+                            panelPosition.y - panelSize.y / 2f,
+                            panelSize.x,
+                            panelSize.y);
+
         if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer)
         {
             if (Input.touchCount > 0)
@@ -57,7 +66,7 @@ public class Qprogram : MonoBehaviour
         {
             if (Input.GetMouseButton(0))
             {
-                virtualKeyPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+                virtualKeyPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
             }
         }
 
@@ -65,23 +74,6 @@ public class Qprogram : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-                if (recognized)
-                {
-                    recognized = false;
-                    strokeId = -1;
-
-                    points.Clear();
-
-                    foreach (LineRenderer lineRenderer in gestureLinesRenderer)
-                    {
-
-                        lineRenderer.SetVertexCount(0);
-                        Destroy(lineRenderer.gameObject);
-                    }
-
-                    gestureLinesRenderer.Clear();
-
-                }
                 ++strokeId;
                 Transform tmpGesture = Instantiate(brushPrefab, transform.position, transform.rotation) as Transform;
                 currentGestureLineRenderer = tmpGesture.GetComponent<LineRenderer>();
@@ -95,47 +87,59 @@ public class Qprogram : MonoBehaviour
             {
                 points.Add(new Point(virtualKeyPosition.x, -virtualKeyPosition.y, strokeId));
 
-                currentGestureLineRenderer.SetVertexCount(++vertexCount);
+                currentGestureLineRenderer.positionCount = ++vertexCount;
                 currentGestureLineRenderer.SetPosition(vertexCount - 1, Camera.main.ScreenToWorldPoint(new Vector3(virtualKeyPosition.x, virtualKeyPosition.y, 10)));
             }
         }
-
-        
     }
-    void OnGUI()
+
+    public void RecognizePattern()
     {
-
-        GUI.Box(drawArea, "Draw Area");
-
-        GUI.Label(new Rect(10, Screen.height - 40, 500, 50), message);
-
-        if (GUI.Button(new Rect(Screen.width - 100, 10, 100, 30), "Recognize"))
+        if (strokeId != -1)
         {
-
-            recognized = true;
-
             Gesture candidate = new Gesture(points.ToArray());
             gestureResult = QPointCloudRecognizer.Classify(candidate, trainingSet.ToArray());
 
-            //message = gestureResult.GestureClass + " " + gestureResult.Score;
-            message = gestureResult;
+            gestureName.SetText(gestureResult);
         }
-
-        GUI.Label(new Rect(Screen.width - 200, 150, 70, 30), "Add as: ");
-        newGestureName = GUI.TextField(new Rect(Screen.width - 150, 150, 100, 30), newGestureName);
-
-        if (GUI.Button(new Rect(Screen.width - 50, 150, 50, 30), "Add") && points.Count > 0 && newGestureName != "")
+        else
         {
+            gestureName.SetText("Kosong");
+        }
+    }
 
-            string fileName = String.Format("{0}/{1}-{2}.xml", Application.persistentDataPath, newGestureName, DateTime.Now.ToFileTime());
-
-#if !UNITY_WEBPLAYER
-            GestureIO.WriteGesture(points.ToArray(), newGestureName, fileName);
-#endif
+    public void GetNewName(string s)
+    {
+        newGestureName = s;
+    }
+    public void AddNewPattern()
+    {
+        Debug.Log(points.Count > 0 && newGestureName != "");
+        if (points.Count > 0 && newGestureName != "")
+        {
+            string fileName = String.Format(Application.dataPath + "/Resources/GestureSet/10-stylus-MEDIUM/{1}-{2}.xml", Application.dataPath, newGestureName, DateTime.Now.ToFileTime());
 
             trainingSet.Add(new Gesture(points.ToArray(), newGestureName));
 
+            GestureIO.WriteGesture(points.ToArray(), newGestureName, fileName);
+
             newGestureName = "";
+            Debug.Log(newGestureName);
+            Debug.Log(newGestureName.ToString());
         }
+    }
+
+    public void DeleteDrawing()
+    {
+        strokeId = -1;
+
+        points.Clear();
+
+        foreach (LineRenderer lineRenderer in gestureLinesRenderer)
+        {
+            lineRenderer.positionCount = 0;
+            Destroy(lineRenderer.gameObject);
+        }
+         gestureLinesRenderer.Clear();
     }
 }
